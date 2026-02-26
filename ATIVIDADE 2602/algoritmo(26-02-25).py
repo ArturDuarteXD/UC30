ade37637927720b2fc91204e68bed8f1185ccb01
ArturDuarteXD <arturduvi@gmail.com>
2026-02-26 14:34:47 -0300

    Add files via upload

---
+++ ATIVIDADE 2602/algoritmo(26-02-25).py
@@ -0,0 +1,7 @@
+aluno = input("Digite seu nome: ") #String é para texto
+periodo = int(input("Digite em que periodo você está: ")) #Inteiro
+ira = float(input("Digite sua IRA: ")) #Float
+
+print(type(aluno))
+print(type(periodo))
+print(type(ira))
