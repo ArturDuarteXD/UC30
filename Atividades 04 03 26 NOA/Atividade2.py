0f06022e75462f229b156e2c9b04ef50fef100ee
ArturDuarteXD <arturduvi@gmail.com>
2026-03-04 11:20:14 -0300

    Add files via upload

---
+++ Atividades 04 03 26 NOA/Atividade2.py
@@ -0,0 +1,6 @@
+lista = ["Sabão", "Desodorante", "Tomate", "Detergente", "Camarão"]
+print("Essa é sua lista: ", lista)
+
+novo_item = input("Digite seu item: ")
+lista.append(novo_item)
+print("Atualizado: ", lista)
---
+++ Atividades 04 03 26 NOA/listas.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+lista =["Cachorro", "Gato"]
+print("Original: ", lista)
+lista.append("Coelho") #Adiciona no fim da lista
+print("Atualizado: ", lista)
+
+lista.insert(1, "Grilo") #Adiciona na posição determinada
+print("Atualizado: ", lista)
+
+lista.extend(["Macaco", "Ovelha"]) # ADiciona mais de um dado por vez
+print("Lista final", lista)
